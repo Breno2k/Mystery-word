fbbe7ce287174e776ea6bedabce4bd427fefc441
Breno Melo <brenolucasmelo@gmail.com>
2025-04-03 10:42:58 -0300

    feat: Finished HTML markup and CSS styling for the game page

--- src/components/MidGame.jsx
+++ src/components/MidGame.jsx
@@ -3,9 +3,30 @@ import './MidGame.css'
 
 const MidGame = ({ verifyLetter }) => {
     return (
-        <div>
-            <h1>MidGame</h1>
-            <button onClick={verifyLetter}>Finalizar jogo</button>
+        <div className="game">
+            <p className="points">
+                <span>pontuation: 000</span>
+            </p>
+            <h1>Adivinhe a palavra: </h1>
+            <h3 className="tip">
+                Dica da palavra: <span>Dica...</span>
+            </h3>
+            <div className="wordContainer">
+                <span className="letter">A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className="letterContainer">
+                <p>Tente adivinhar a palavra: </p>
+                <form action="">
+                    <input type="text" name="letter" maxLength="1" required />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já ultilizadas:</p>
+                <span>A, </span>
+                <span>B, </span>
+            </div>
         </div>
     )
 }
