6640e725444f00eb24e26bc384b430f9110a96b4
Breno Melo <brenolucasmelo@gmail.com>
2025-09-23 22:10:43 -0300

    feat: configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Mystery-word",
   server: {
     open: true,
     hmr: false
