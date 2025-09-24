118754c2edec9cb87e63539c973e810820e29ada
Breno Melo <brenolucasmelo@gmail.com>
2025-09-23 21:13:09 -0300

    fix: preenchendo wordList

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -6,4 +6,14 @@ export const wordsList = {
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
     alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leão", "Pássaro"],
+    país: ["Brasil", "Canadá", "Japão", "Itália", "Egito"],
+    cor: ["Vermelho", "Azul", "Amarelo", "Verde", "Preto"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Vôlei"],
+    profissão: ["Médico", "Professor", "Engenheiro", "Advogado", "Designer"],
+    objeto: ["Mesa", "Cadeira", "Livro", "Celular", "Lâmpada"],
+    roupa: ["Camiseta", "Calça", "Jaqueta", "Saia", "Sapato"],
+    cidade: ["Paris", "Nova York", "Tóquio", "Londres", "Sydney"],
+    música: ["Guitarra", "Bateria", "Piano", "Violino", "Flauta"],
+    tecnologia: ["Computador", "Internet", "Robô", "Smartphone", "Drones"]
 };
